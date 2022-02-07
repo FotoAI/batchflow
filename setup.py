dc918095aea66c52d6de00a7a17491146604065c
tushr <tusharkolhe@gofynd.com>
2022-02-07 13:43:22 +0530

    Fix version

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
         "loguru==0.5.3",
         "Pillow>=6.2.0",
         "shapely==1.7.0",
-        "requests==2.23.0",
+        "requests>=2.23.0",
         "catalogue==2.0.0",
         "boto3==1.12.18",
     ],
